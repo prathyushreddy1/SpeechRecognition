b2b16d7ce7bd2d77574d1e94631cdfbca6ee421d
prathyushreddy1 <prathyushreddylpr@gmail.com>
2022-08-22 08:45:05 +0530

    Add files via upload

---
+++ asr_1.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  7 11:33:51 2019
+
+@author: Mandy
+"""
+
+from preprocess import *
+import keras
+import pickle
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.utils import to_categorical
+from numpy import newaxis
+
+# Second dimension of the feature is dim2
+feature_dim_2 = 11
+
+# Save data to array file first
+save_data_to_array(max_len=feature_dim_2)
+
+# # Loading train set and test set
+X_train, X_test, y_train, y_test = get_train_test()
+
+# # Feature dimension
+feature_dim_1 = 20
+channel = 1
+epochs = 50
+batch_size = 100
+verbose = 1
+num_classes = 36
+
+# Reshaping to perform 2D convolution
+X_train = X_train.reshape(X_train.shape[0], feature_dim_1, feature_dim_2, channel)
+X_test = X_test.reshape(X_test.shape[0], feature_dim_1, feature_dim_2, channel)
+
+y_train_hot = to_categorical(y_train)
+y_test_hot = to_categorical(y_test)
+
+def get_model():
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(2, 2), activation='relu', input_shape=(feature_dim_1, feature_dim_2, channel)))
+    model.add(Conv2D(48, kernel_size=(2, 2), activation='relu'))
+    model.add(Conv2D(120, kernel_size=(2, 2), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.25))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.4))
+    model.add(Dense(num_classes, activation='softmax'))
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),
+                  metrics=['accuracy'])
+    return model
+
+# Predicts one sample
+def predict(filepath, model):
+    sample = wav2mfcc(filepath)
+    sample_reshaped = sample.reshape(1, feature_dim_1, feature_dim_2, channel)
+    return get_labels()[0][
+            np.argmax(model.predict(sample_reshaped))
+    ]
+    
+model = get_model()
+model.fit(X_train, y_train_hot, batch_size=batch_size, epochs=epochs, verbose=verbose, validation_data=(X_test, y_test_hot))
+
+
+wavfileinp = "./in.wav/"
+mfcc = wav2mfcc(wavfileinp, 11)
+
+x=mfcc
+b = x[newaxis, ..., newaxis ]
+b.shape
+
+path="./data/"
+labels, _, _ = get_labels(path)
+labels[np.argmax(model.predict(b))]
+
